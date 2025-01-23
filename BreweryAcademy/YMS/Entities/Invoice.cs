b26b87e4e8625f868b8a0fb3df95f2295362a076
Gabriel Fernandes <114429621+GabrielFer555@users.noreply.github.com>
2025-01-23 11:43:42 -0300

    feat: added main classes

--- BreweryAcademy/YMS/Entities/Invoice.cs
+++ BreweryAcademy/YMS/Entities/Invoice.cs
@@ -1,6 +1,12 @@
-﻿namespace YMS.Entities
+﻿using YMS.Enums;
+
+namespace YMS.Entities
 {
 	public class Invoice
 	{
+        public int Id { get; set; }
+        public int InvoiceKey { get; set; }
+        public List<InvoiceItem> Items { get; set; } = new();
+        public InvoiceType InvoiceType { get; set; }
 	}
 }
---
+++ BreweryAcademy/YMS/Entities/InvoiceItem.cs
@@ -0,0 +1,9 @@
+﻿namespace YMS.Entities
+{
+	public class InvoiceItem
+	{
+        public int Id { get; set; }	
+		public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
