6b457ba35808ac49cb1bd57922248719f407a389
BiaLanser <biancalperes@gmail.com>
2025-01-23 14:19:08 -0300

    feat: wms entities

---
+++ BreweryAcademy/WMS/Entities/Stock.cs
@@ -0,0 +1,12 @@
+﻿using WMS.Enums;
+
+namespace WMS.Entities
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public OperationType OperationType { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
