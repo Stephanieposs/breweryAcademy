ffddbdd6439d1b91a94d836ef94ec3fb4c901137
Stephanie Possamai <stephaniepossamaiamanda@gmail.com>
2025-01-23 17:39:22 -0300

    'SapApi'

---
+++ BreweryAcademy/SAP4/Migrations/20250123183711_SAPSecondMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SAP4.Migrations
+{
+    /// <inheritdoc />
+    public partial class SAPSecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_InvoiceSAPs",
+                table: "InvoiceSAPs");
+
+            migrationBuilder.RenameTable(
+                name: "InvoiceSAPs",
+                newName: "InvoiceSAP");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_InvoiceSAP",
+                table: "InvoiceSAP",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_InvoiceSAP",
+                table: "InvoiceSAP");
+
+            migrationBuilder.RenameTable(
+                name: "InvoiceSAP",
+                newName: "InvoiceSAPs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_InvoiceSAPs",
+                table: "InvoiceSAPs",
+                column: "Id");
+        }
+    }
+}
---
+++ BreweryAcademy/SAP4/Repositories/ISapRepository.cs
@@ -0,0 +1,11 @@
+﻿using SAP4.Entities;
+
+namespace SAP4.Repositories;
+
+public interface ISapRepository
+{
+    Task<IEnumerable<InvoiceSAP>> GetAllInvokes();
+    Task<InvoiceSAP> GetById(int id);
+    Task<InvoiceSAP> AddInvoice(InvoiceSAP invoice);
+    Task<InvoiceSAP> UpdateInvoiceStatus(InvoiceSAP invoice);
+}
