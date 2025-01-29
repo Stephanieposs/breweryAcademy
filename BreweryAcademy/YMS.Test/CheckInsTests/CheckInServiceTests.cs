efa0b961fc43291eb69b971c8b7bfd5f33972bb7
Gabriel Fernandes <114429621+GabrielFer555@users.noreply.github.com>
2025-01-29 13:12:27 -0300

    test:started tests

---
+++ BreweryAcademy/YMS.Test/CheckInsTests/CheckInServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMS.Test.CheckIns
+{
+	public class CheckInServiceTests
+	{
+
+		[Fact]
+		public async Task CheckInService_CreateCheckIn_ReturnsObject()
+		{
+
+		}
+	}
+}
